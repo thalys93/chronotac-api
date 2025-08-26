970b731841cae30dfa16c6031ef59e0efa8ef6b7
Thalys93 <luisthalys1@hotmail.com>
2025-08-26 20:22:35 -0300

    feat: deploy configuration

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,18 +5,29 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const AppDataSource = new DataSource({
+// Parse DATABASE_URL se disponível
+let config: any = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_DATABASE || 'chronotac',
   synchronize: false,
   logging: false,
   entities: [Game],
-  migrations: ['src/database/migrations/*.ts'],
+  migrations: ['dist/database/migrations/*.js'],
   subscribers: [],
   migrationsTableName: 'migrations_history',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-});
+};
+
+if (process.env.DATABASE_URL) {
+  config.url = process.env.DATABASE_URL;
+  config.ssl = { rejectUnauthorized: false };
+} else {
+  config = {
+    ...config,
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_DATABASE || 'chronotac',
+  };
+}
+
+export const AppDataSource = new DataSource(config);
