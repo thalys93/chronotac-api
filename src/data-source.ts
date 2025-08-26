e0bb9e76bc884826b97e41cbd47ee3eab9a21f2c
Thalys93 <luisthalys1@hotmail.com>
2025-08-26 20:19:33 -0300

    feat: start to create prod branch

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,16 +6,17 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USERNAME || 'chronotac',
-  password: process.env.DB_PASSWORD || 'chronotac123',
-  database: process.env.DB_DATABASE || 'tictac',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'chronotac',
   synchronize: false,
   logging: false,
   entities: [Game],
-  migrations: ['src/database/migrations/*.ts'], 
+  migrations: ['src/database/migrations/*.ts'],
   subscribers: [],
   migrationsTableName: 'migrations_history',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
