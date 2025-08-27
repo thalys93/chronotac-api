e3f9cedbac76b4fb21b68ac96df9f824d52d46eb
Thalys93 <luisthalys1@hotmail.com>
2025-08-26 21:04:19 -0300

    refact: adjusts in migrations and compose

---
+++ src/database/migrations/1756253034882-CreateGameTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateGameTable1756253034882 implements MigrationInterface {
+    name = 'CreateGameTable1756253034882'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "games" ("id" SERIAL NOT NULL, "board" json NOT NULL, "winner" text, "isDraw" boolean NOT NULL DEFAULT false, "currentPlayer" character varying NOT NULL, "playedDate" TIMESTAMP NOT NULL DEFAULT now(), "status" text NOT NULL, CONSTRAINT "PK_c9b16b62917b5595af982d66337" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "games"`);
+    }
+
+}
